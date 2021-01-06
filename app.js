5d59e984ad7119f2e1ab59465059942061f24f55
Maylen Nunez <maylennunez@hotmail.com>
2021-01-06 17:04:13 -0500

    to do list

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+let items = [];
 const app = express();
 
-
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay)
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
   }
 
+let day = today.toLocaleDateString("en-US", options)
+
+
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day, newListItems: items
   });
+
+});
+
+app.post("/", function(req, res){
+  let item = req.body.newItems;
+  //console.log(item);
+  items.push(item);
+res.redirect("/");
 
 });
 
